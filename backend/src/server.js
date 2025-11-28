047b1f7eaf2c522eeb55395b02c55bbe9f1bad2f
Greeshman Dama <154655367+greeshmandama5559@users.noreply.github.com>
2025-11-28 17:51:43 +0530

    setup the frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,14 @@ import authRouter from './routes/auth.route.js';
 import messageRouter from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",  // React dev server
+  credentials: true                 // allow cookies
+}));
 
 app.use(express.json());
 app.use(cookieParser());
