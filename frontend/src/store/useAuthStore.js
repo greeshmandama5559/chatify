9d0295011baa5cb0f01487ab84ffa19e7303ddff
Greeshman Dama <154655367+greeshmandama5559@users.noreply.github.com>
2025-11-29 12:35:53 +0530

    login functionaility added

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: true,
   isSigningUp: false,
+  isLoggingIn: false,
 
   checkAuth: async () => {
     try {
@@ -43,6 +44,40 @@ export const useAuthStore = create((set) => ({
       }
     } finally {
       set({ isSigningUp: false });
+    }
+  },
+
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstace.post("/auth/login", data);
+      set({ authUser: res.data });
+
+      toast.success("successfully logged in");
+    } catch (error) {
+      if (error?.response?.data?.message) {
+        toast.error(error.response.data.message);
+        console.log("Error in login page: ", error.response.data);
+      } else if (error?.message) {
+        toast.error(error.message);
+        console.log("Error in login page (network/CORS): ", error);
+      } else {
+        toast.error("An unknown error occurred");
+        console.log("Error in login page (unknown): ", error);
+      }
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstace.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logout successfull");
+    } catch (error) {
+      console.log("Logout Error: " + error);
+      toast.error("Logout Failed, Try later");
     }
   },
 }));
