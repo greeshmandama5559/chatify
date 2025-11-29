4f12b754fc53ed2e6bd715dd2e52feba3a74b64c
Greeshman Dama <154655367+greeshmandama5559@users.noreply.github.com>
2025-11-29 11:00:52 +0530

    implemented signup page in frontend and signup data stores in database

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",  // React dev server
+  origin: ENV.CLIENT_URL,  // React dev server
   credentials: true                 // allow cookies
 }));
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,48 @@
-import { create } from 'zustand';
+import { create } from "zustand";
+import { axiosInstace } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
-    authUser: null,
-}))
+  authUser: null,
+  isCheckingAuth: true,
+  isSigningUp: false,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstace.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log(
+        "Error in check Auth: ",
+        error?.response ?? error?.message ?? error
+      );
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstace.post("/auth/signup", data);
+      set({ authUser: res.data });
+
+      toast.success("Account created successfully");
+    } catch (error) {
+      if (error?.response?.data?.message) {
+        toast.error(error.response.data.message);
+        console.log("Error in signup page: ", error.response.data);
+      } else if (error?.message) {
+        // Network errors, CORS, etc
+        toast.error(error.message);
+        console.log("Error in signup page (network/CORS): ", error);
+      } else {
+        toast.error("An unknown error occurred");
+        console.log("Error in signup page (unknown): ", error);
+      }
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+}));
