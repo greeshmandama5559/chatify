b5bef997158094652972c3d61b0f5f591f360970
Greeshman Dama <154655367+greeshmandama5559@users.noreply.github.com>
2025-12-30 20:50:55 +0530

    commented frontend path in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,15 +28,15 @@ app.use('/api/auth', authRouter);
 app.use('/api/messages', messageRouter);
 app.use('/api/profile', profile);
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
     connectDB();
